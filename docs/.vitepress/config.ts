9f1c5639de994b2660233da7a806e36377909614
Lino <60299904+surmatik@users.noreply.github.com>
2023-06-29 14:02:07 +0200

    Setup Vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Sparrows Ideen",
+  description: "Tweet & Shark",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Spiel Ideen', link: '/spiel-ideen/Allgemein' },
+      { text: 'Programm Ideen', link: '/Programm-Ideen/Allgemein' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Spiel Ideen',
+        collapsible: true,
+        collapsed: true,
+        items: [
+          { text: 'Allgemein', link: '/Spiel-Ideen/Allgemein.md' },
+          { text: 'Fangis', link: '/Spiel-Ideen/Fangis.md' },
+          { text: 'Fussball', link: '/Spiel-Ideen/Fussball.md' },
+          { text: 'Games IRL', link: '/Spiel-Ideen/Games-IRL.md' },
+          { text: 'Hosensackspiele', link: '/Spiel-Ideen/Hosensackspiele.md' },
+          { text: 'Rätsel', link: '/Spiel-Ideen/Raetsel.md' },
+          { text: 'Stafetten', link: '/Spiel-Ideen/Stafetten.md' },
+          { text: 'Völks', link: '/Spiel-Ideen/Voelks.md' }
+        ]
+      },
+      {
+        text: 'Programm Ideen',
+        collapsible: true,
+        collapsed: true,
+        items: [
+          { text: 'Allgemein', link: '/Programm-Ideen/Allgemein.md' },
+          { text: 'Basteln', link: '/Programm-Ideen/Basteln.md' },
+          { text: 'Bauen', link: '/Programm-Ideen/Bauen.md' },
+          { text: 'Kochen und Backen', link: '/Programm-Ideen/Kochen-und-Backen.md' },
+          { text: 'Lernen', link: '/Programm-Ideen/Lernen.md' }
+        ]
+      }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/surmatik/SparrowsIdeen/docs/:path',
+      text: 'bearbeiten'
+    },
+
+    footer: {
+      copyright: 'Copyright © 2023 Tweet & Shark'
+    },
+
+    socialLinks: [
+      { icon: 'instagram', link: 'https://www.instagram.com/cevi.wetzikon/' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,22 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Sparrows Ideen"
+  tagline: von Tweet & Shark
+  actions:
+    - theme: brand
+      text: Nach einer Idee suchen
+      link: /Spiel-Ideen/Allgemein
+    - theme: alt
+      text: Cevi Wiki
+      link: https://wiki.cevi.ch/
+
+features:
+  - title: Spiel Ideen
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Programm Ideen
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
